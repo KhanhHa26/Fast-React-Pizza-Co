c96dd34bacea38ce6e13071a42993a7f7877e42a
Khanh Ha <84813907+KhanhHa26@users.noreply.github.com>
2024-01-16 15:41:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,151 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+//Pizza data object containing several pizza objects
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      {" "}
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+//writing component as functions
+//needs to return some markup, usually in the form of JSX
+//can only return one block of jsx so we have to put everything in div
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+//pasing data from menu to pizza
+function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+
+      {numPizzas > 0 && (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious!
+          </p>
+          <ul className="pizzas">
+            {/*for each pizza in pizzaData, creating Pizza object*/}
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObject={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      )}
+    </main>
+  );
+}
+
+function Pizza({ pizzaObject }) {
+  //   if (pizzaObject.soldOut) return null;
+
+  return (
+    <li className={`pizza ${pizzaObject.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObject.photoName} alt={pizzaObject.name} />
+      <div>
+        <h3>{pizzaObject.name}</h3>
+        <p>{pizzaObject.ingredients}</p>
+        <span>{pizzaObject.soldOut ? "SOLD OUT" : pizzaObject.price}</span>
+        {pizzaObject.soldOut ? null : <btn className="btnAdd">Add to cart</btn>}
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  //   if (!isOpen) return <p>We are happy to welcome you between {openHour} PM and {closeHour} PM.</p>
+  return (
+    <footer className="footer">
+      {isOpen && <Order openHour={openHour} closeHour={closeHour} />}
+    </footer>
+  );
+}
+
+function Order({ openHour, closeHour }) {
+  return (
+    <div className="order">
+      <br></br>
+      <p>
+        We are open from {openHour} PM to {closeHour} PM.
+      </p>
+      <p>Come visit us or order online!</p>
+      <btn className="btn">Order Now</btn>
+    </div>
+  );
+}
+
+//render to the dom
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {" "}
+    <App />{" "}
+  </React.StrictMode>
+);
